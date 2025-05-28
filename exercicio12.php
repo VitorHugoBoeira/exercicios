22a1f3ce4f91a015a0560dc350479c5d918af437
VitorHugoBoeira <boeiravitorhugo@gmail.com>
2025-05-27 21:48:37 -0300

    descubra se o número e positivo, negativo ou zero

---
+++ exercicio12.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = readline("Digite um numero para saber se ele é positivo, negativo ou zero: ");
+
+if ($a>0){
+	echo "o número $a é positivo!";
+}
+
+elseif ($a<0){
+	echo "o número $a é negativo!";
+}
+
+else {
+	echo "o número $a é zero!";
+}
+
+
+?>
