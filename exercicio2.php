608411cd8ecf04d92279dbe2c53926fab2d0ca99
VitorHugoBoeira <boeiravitorhugo@gmail.com>
2025-05-27 21:05:46 -0300

    calculador media de 3 numeros

--- exercicio2.php
+++ exercicio2.php
@@ -5,7 +5,7 @@ $b = readline("digite segundo valor: ");
 $c = readline("digite o terceiro valor: ");
 $media = 0;
 
-$media = ($a + $b + $c) /3;
+$media = ($a + $b + $c) /3 ;
 
 echo "a media dos numeros são $media";
 
