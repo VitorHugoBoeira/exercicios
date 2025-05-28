0c237962d6b2ecd78cc23fe8165e4d93dfd554ed
VitorHugoBoeira <boeiravitorhugo@gmail.com>
2025-05-27 22:16:27 -0300

    descubra o dia da semana

---
+++ exercicio14.php
@@ -0,0 +1,39 @@
+<?php
+
+echo "descubra o dia da semana!";
+
+$a = readline("Digite o numero(digite de 1 a 7, sendo 1 domingo e 7 sábado!): ");
+
+switch($a){
+
+case 1:
+	echo "o dia da semana e domingo!";
+	break;
+
+case 2:
+	echo "o dia da semana e segunda!";
+	break;
+
+case 3:
+	echo "o dia da semana e terça!";
+	break;
+
+case 4:
+	echo "o dia da semana e quarta!";
+	break;
+
+case 5:
+	echo "o dia da semana e quinta!";
+	break;
+
+case 6:
+	echo "o dia da semana e sexta!";
+	break;
+
+case 7:
+	echo "o dia da semana e sábado!";
+	break;
+
+}
+
+?>
