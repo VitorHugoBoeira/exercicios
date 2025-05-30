465d1a3575626052678de3848339dc9dabef4cdf
VitorHugoBoeira <boeiravitorhugo@gmail.com>
2025-05-29 21:05:04 -0300

    maior numero com loop

---
+++ exercicio22.php
@@ -0,0 +1,12 @@
+<?php
+
+for ($i=0; $i <=5; $i++){
+	$numero = readline("Digite um numero para descobrir qual o maior entre eles: ");
+	$maior;	
+	if($numero > $maior){ 
+		$maior = $numero;
+	}
+}
+echo "o maior número é: $maior";
+
+?>
