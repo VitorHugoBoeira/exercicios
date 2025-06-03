3156d6b56f2e52075a67c4eb9f95b978636946b3
VitorHugoBoeira <boeiravitorhugo@gmail.com>
2025-06-03 20:03:19 -0300

    adivinhe o numero aleatorio com dica

--- exercicio26.php
+++ exercicio26.php
@@ -3,8 +3,11 @@
 $numeroaleatorio = rand(1, 100);
 do {
 	$numero = readline("Tente adivinhar um numero de 1 a 100: ");
-	if ($numero !== $numeroaleatorio){
-		echo "\tTente novamente!\n";
+	if ($numero > $numeroaleatorio){
+		echo "\tO numero é menor que o selecionado!\n";
+	}
+	if ($numero < $numeroaleatorio){
+		echo "\tO numero é maior que o selecionado!\n"; 
 	}
 }
 while ($numero != $numeroaleatorio);
